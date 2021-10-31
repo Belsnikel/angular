d3c75bdfa59ebb48c22bcc29eb67d06f4c40eb5a
Belsnikel <afterouy@gmail.com>
2021-10-31 15:29:37 +0300

    created heroes component & hero module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
-import { PostComponent } from './post/post.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import {FormsModule} from "@angular/forms"; // <--NgModel lives here
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    PostComponent
+    HeroesComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
